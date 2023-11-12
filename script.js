fdd4b144399b1065379fcd5392482b3429060e38
Yuri Metodiev <juri1602@gmail.com>
2023-11-12 11:20:40 +0200

    1.2

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.querySelector('.display'); // display element
+const buttons = document.querySelectorAll('button'); // select all buttons   
+const specialChars = ["%", "*", "-", "/","+", "="]
+let output = ""; // output string
+
+const calculate = (btnValue) => {
+    if(btnValue === "=" && btnValue !=="") {
+        //if output has %
+        output = eval(output.replace("%", "/100"));
+    }
+    else if(btnValue === "AC") {
+    output = "";
+    } 
+    else if (btnValue === "DEL"){
+        
+    }
+    
+    
+    display.value = output;
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value))
+}); // add event listener to each button    
